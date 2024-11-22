b987dac1db430d6baa0d993ad8f7c3d2b2563200
dconco <onipededavid208@gmail.com>
2024-11-22 20:18:40 +0100

    completed pricing

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,6 +16,7 @@ Route::view(['/', '/index'], '::App')->name('index');
 Route::view('/about', '::About')->name('about');
 Route::view('/skills', '::Skills')->name('skills');
 Route::view('/contact', '::Contact')->name('contact');
-Route::view('/experience', '::Experience')->name('experience');
+Route::view('/pricing', '::Pricing')->name('pricing');
+Route::view('/projects', '::Projects')->name('projects');
 Route::view('/portfolio', '::Portfolio')->name('portfolio');
 Route::any('*', view::render('::Errors::NotFound'));
