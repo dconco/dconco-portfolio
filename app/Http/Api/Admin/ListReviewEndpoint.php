36f586cc51d57e55f74c929bd96863fe01589ae8
dconco <onipededavid208@gmail.com>
2024-12-01 15:13:25 +0100

    added admin api

---
+++ app/Http/Api/Admin/ListReviewEndpoint.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Api\Admin;
+
+use PhpSlides\Http\Request;
+use PhpSlides\Http\ApiController;
+
+final class ListReviewEndpoint extends ApiController
+{
+	public function index(Request $req)
+	{
+		return '';
+	}
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,13 @@
 use PhpSlides\Http\Api;
 use App\Http\Api\ContactEndpoint;
 use App\Http\Api\AddReviewEndpoint;
+use App\Http\Api\Admin\ListReviewEndpoint;
 
 Api::v1()->route(url: '/contact', controller: ContactEndpoint::class);
 Api::v1()->route(url: '/review/add', controller: AddReviewEndpoint::class);
+
+$admin_review = Api::v1()->define('/admin/review', ListReviewEndpoint::class);
+
+$admin_review->map([
+	'/list' => [POST, '@list'],
+]);
