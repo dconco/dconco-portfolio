47dd74fb846ec9a7fa0b81fafe38115fce131e01
dconco <onipededavid208@gmail.com>
2024-12-01 19:34:37 +0100

    refactor AdminGuard authorization to use environment variables for admin credentials

--- app/Guards/AdminGuard.php
+++ app/Guards/AdminGuard.php
@@ -7,12 +7,19 @@ use PhpSlides\Http\Auth\AuthGuard;
 
 final class AdminGuard extends AuthGuard
 {
-	public function authorize(): bool
+	public function authorize (): bool
 	{
 		$auth = self::$request->auth()->basic;
 
-		if (isset($auth['username']) && $auth['username'] === 'd') {
-			return true;
+		if (isset($auth['username']) && isset($auth['password']))
+		{
+			$admin_username = getenv('ADMIN_USERNAME');
+			$admin_password = getenv('ADMIN_PASSWORD');
+
+			if ($auth['username'] === $admin_username && password_verify($auth['password'], $admin_password))
+			{
+				return true;
+			}
 		}
 
 		http_response_code(401);
